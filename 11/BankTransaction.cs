ea5346fcd57fbed64a520e3f92aaa32868718ed5
Индира Шакирова <IndIShakirova@stud.kpfu.ru>
2021-11-26 20:37:49 +0300

    add lab11

---
+++ 11/BankTransaction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab
+{
+    class BankTransaction
+    {
+        public enum TypeTransaction
+        {
+            Withdrawal,
+            Replenishment,
+            Transfer
+        }
+        public readonly DateTime dateTime = new DateTime();
+        public readonly decimal summ;
+        public readonly TypeTransaction typeTransaction;
+        internal BankTransaction(decimal summ, TypeTransaction type)
+        {
+            this.summ = summ;
+            dateTime = DateTime.Now;
+            typeTransaction = type;
+        }
+
+    }
+}
+
